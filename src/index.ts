2a78ecb34a5abc4ec8f796bf37259583ddb60125
0xgareth <gareth@garethv.xyz>
2022-05-09 16:02:50 +0100

    index ts changes

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { ApolloServer } from "apollo-server-express";
+import * as Express from "express";
+
+const main = async () => {
+
+    const apolloServer = new ApolloServer({});
+
+}
+
+main();
