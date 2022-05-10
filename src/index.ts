345635547d3a70e3cde2099f2b1f7aac0656dcc7
0xgareth <gareth@garethv.xyz>
2022-05-10 13:15:32 +0100

    user entity, user resolver, postgres orm config

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,16 @@
 import { ApolloServer } from "apollo-server-express";
 import Express from "express";
-import { buildSchema, Resolver, Query } from "type-graphql";
+import { buildSchema } from "type-graphql";
 import "reflect-metadata";
+import { createConnection } from "typeorm";
 
-@Resolver()
-class HelloResolver {
-  
-  @Query(() => String, { nullable: true,  }) 
-  async hello() {
-    return "Hello world!";
-  }
-}
+import { RegisterResolver } from "./modules/user/Register";
 
 const main = async () => {
+    await createConnection();
 
     const schema = await buildSchema({
-        resolvers: [HelloResolver],
+        resolvers: [RegisterResolver],
     });
 
     const apolloServer = new ApolloServer({ schema });
